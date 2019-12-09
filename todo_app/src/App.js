1a4138924885415894a23207ca324c24e07b3791
Renan Pallin <renanpallin@gmail.com>
2019-12-09 00:47:27 -0300

    [todo_app] Input controlado

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 export default class TodoApp extends React.Component {
+  state = {
+    inputValue: 'mudando aqui'
+  }
+
+  onChangeInput(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+  
   render() {
     return (
       <div>
-        <h1>Nosso todoApp</h1>
-        <input />
+        <h1>Todo App</h1>
+        <input
+          value={this.state.inputValue}
+          onChange={e => this.onChangeInput(e)} />
         <button>salvar</button> 
       </div>
     );
