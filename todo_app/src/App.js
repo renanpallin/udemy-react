b423c5cf10d5431fd694b5f82dcd53be97fa726c
Renan Pallin <renanpallin@gmail.com>
2019-12-09 00:03:36 -0300

    [todo_app] Projeto limpo e input na tela

---
+++ todo_app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Nosso todoApp</h1>
+        <input />
+        <button>salvar</button> 
+      </div>
+    );
+  }
+}
