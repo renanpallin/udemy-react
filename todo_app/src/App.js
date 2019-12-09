8de6963c530b88df76e4736f2ea68d4ce8783611
Renan Pallin <renanpallin@gmail.com>
2019-12-09 00:57:49 -0300

    [todo_app] Listando todos estáticos

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 export default class TodoApp extends React.Component {
   state = {
-    inputValue: 'mudando aqui'
+    inputValue: '',
+    todos: [
+      "lavar a louça",
+      "estudar react",
+      "Dar 5 estrelas no curso"
+    ],
   }
 
   onChangeInput(e) {
@@ -12,13 +17,20 @@ export default class TodoApp extends React.Component {
   }
   
   render() {
+    const { inputValue, todos } = this.state;
     return (
       <div>
         <h1>Todo App</h1>
         <input
-          value={this.state.inputValue}
+          value={inputValue}
           onChange={e => this.onChangeInput(e)} />
-        <button>salvar</button> 
+        <button>salvar</button>
+
+        <ul>
+          {todos.map((todo, index) => {
+            return <li key={index}>{todo}</li>
+          })}
+        </ul>
       </div>
     );
   }
