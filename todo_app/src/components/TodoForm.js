acb5425e5abdde893472b01d8fc0fd10a06e22e9
Renan Pallin <renanpallin@gmail.com>
2019-12-10 12:29:41 -0300

    [todo_app] Melhorando ui do TodoForm

--- todo_app/src/components/TodoForm.js
+++ todo_app/src/components/TodoForm.js
@@ -4,17 +4,25 @@ import { setText, addTodo } from '../actions';
 
 const TodoForm = ({ text, setText, addTodo }) => {
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      addTodo(text);
-    }}>
-      <input
-        value={text}
-        onChange={e => setText(e.target.value)} />
-      <button
-        className="btn">
-          salvar
-      </button>
+    <form
+      className="row"
+      onSubmit={(e) => {
+        e.preventDefault();
+        addTodo(text);
+      }}>
+      <div className="input-field col s10">
+        <input
+          id="todo-input"
+          type="text"
+          value={text}
+          onChange={e => setText(e.target.value)} />
+          <label htmlFor="todo-input">Novo todo</label>
+      </div>
+      <div className="input-field col s2">
+        <button className="btn waves-effect waves-light">
+            salvar
+        </button>
+      </div>
     </form>
   );
 }
